ff78299ab405a9506a6b56c3596012c34fa4eec6
kinga <kinga.kwasniak5@gmail.com>
2026-03-23 14:19:47 +0100

    init.sh in mariadb fixed

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-mkdir -p /var/run/mysqld
-chown -R mysql:mysql /var/run/mysqld
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Inicjalizacja bazy MariaDB..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
 mariadbd-safe --datadir='/var/lib/mysql' --skip-networking &
 
-echo "Czekam na uruchomienie serwisu..."
-until mariadb-admin ping; do
+until mariadb-admin --socket=/run/mysqld/mysqld.sock ping; do
+    echo "Czekam na uruchomienie MariaDB..."
     sleep 2
 done
 
-echo "Konfiguracja użytkowników..."
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mariadb -e "FLUSH PRIVILEGES;"
+mariadb -u root << EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
 
-echo "Restartowanie MariaDB w trybie normalnym..."
-mariadb-admin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+mariadb-admin -u root -p${MYSQL_ROOT_PASSWORD} --socket=/run/mysqld/mysqld.sock shutdown
 
 exec mariadbd --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0
